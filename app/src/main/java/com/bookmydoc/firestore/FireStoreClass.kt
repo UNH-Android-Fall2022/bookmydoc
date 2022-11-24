c92effbecfb9f7389930b4064b8b282f6f42a245
Smith <sam@example.com>
2022-11-24 11:18:18 -0500

    latestcode

---
+++ app/src/main/java/com/bookmydoc/firestore/FireStoreClass.kt
@@ -0,0 +1,311 @@
+package com.bookmydoc.firestore
+
+import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
+import android.net.Uri
+import android.util.Log
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.bookmydoc.model.*
+import com.bookmydoc.Constants
+import com.bookmydoc.Constants.BOOKING
+import com.bookmydoc.view.*
+import com.google.firebase.auth.FirebaseAuth
+
+class FireStoreClass {
+
+    private val mFireStore = FirebaseFirestore.getInstance()
+
+    fun registerUser(activity: RegisterActivity, user: User) {
+        mFireStore.collection(Constants.USERS)
+            .document(user.id)
+            .set(user, SetOptions.merge())
+            .addOnSuccessListener {
+                activity.userRegistrationSuccess()
+            }.addOnFailureListener { exception ->
+                activity.hideProgressDialog()
+                Log.e(activity.javaClass.simpleName, exception.message.toString())
+            }
+
+    }
+
+    fun addDoctor(activity: DashboardActivity, user: Doctors) {
+        mFireStore.collection("doctors")
+            .document(user.doctor_id)
+            .set(user, SetOptions.merge())
+            .addOnSuccessListener {
+                activity.userRegistrationSuccess()
+            }.addOnFailureListener { exception ->
+                activity.hideProgressDialog()
+                Log.e(activity.javaClass.simpleName, exception.message.toString())
+            }
+
+    }
+
+    fun getCurrentUserId(): String {
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        var currentUserId = ""
+        if (currentUser != null) {
+            currentUserId = currentUser.uid
+        }
+        return currentUserId
+    }
+
+
+    fun getUserDetails(activity: Activity) {
+        mFireStore.collection(Constants.USERS)
+            .document(getCurrentUserId())
+            .get()
+            .addOnSuccessListener { document ->
+                val user = document.toObject(User::class.java)
+                if (user != null) {
+
+                    val sharedPref = activity.getSharedPreferences(
+                        Constants.MY_SHOP_PAL_PREFERENCES,
+                        Context.MODE_PRIVATE
+                    )
+
+                    val editor: SharedPreferences.Editor = sharedPref.edit()
+                    editor.putString(
+                        Constants.LOGGED_IN_USERNAME,
+                        "${user.fullName}"
+                    )
+                    editor.apply()
+
+                    when (activity) {
+                        is LoginActivity -> activity.userLoggedInSuccess(user)
+                        is RegisterActivity -> activity.userLoggedInSuccess(user)
+                        is SettingsActivity -> activity.userDetailSuccess(user)
+                        is DashboardActivity -> activity.userDetailSuccess(user)
+                    }
+                }
+
+            }.addOnFailureListener { exception ->
+                when (activity) {
+                    is LoginActivity -> activity.hideProgressDialog()
+                    is RegisterActivity -> activity.hideProgressDialog()
+                    is SettingsActivity -> activity.hideProgressDialog()
+                }
+                Log.e(activity.javaClass.simpleName, exception.message.toString())
+            }
+    }
+
+    fun updateUserProfileData(activity: Activity, userHashMap: HashMap<String, Any>) {
+
+        mFireStore.collection(Constants.USERS)
+            .document(getCurrentUserId())
+            .update(userHashMap)
+            .addOnSuccessListener {
+                when (activity) {
+                    is UserProfileActivity -> activity.userProfileUpdateSuccess()
+                }
+            }.addOnFailureListener { e ->
+                when (activity) {
+                    is UserProfileActivity -> activity.hideProgressDialog()
+                }
+                Log.e(activity.javaClass.simpleName, "Error while updating profile", e)
+            }
+    }
+
+    fun uploadImageToCloudStorage(activity: Activity, imageFileUri: Uri?, imageType: String) {
+        val shrf: StorageReference = FirebaseStorage.getInstance().reference.child(
+            imageType
+                    + System.currentTimeMillis() + "."
+                    + Constants.getFileExtension(activity, imageFileUri)
+        )
+
+        shrf.putFile(imageFileUri!!)
+            .addOnSuccessListener { snapShot ->
+                Log.e("Firebase Image Url", snapShot.metadata!!.reference!!.downloadUrl.toString())
+                snapShot.metadata!!.reference!!.downloadUrl
+                    .addOnSuccessListener { uri ->
+                        Log.e("Image Url", uri.toString())
+
+                        when (activity) {
+                            is UserProfileActivity -> activity.imageUploadSuccess(uri.toString())
+                        }
+                    }
+            }.addOnFailureListener { e ->
+                when (activity) {
+                    is UserProfileActivity -> activity.hideProgressDialog()
+                }
+                Log.e("Error while uploading", "Error while uploading image to db", e)
+            }
+    }
+
+    fun getCategoriesList(activity: Activity) {
+        mFireStore.collection(Constants.CATEGORIES)
+            .get()
+            .addOnSuccessListener { document ->
+                Log.i(activity.javaClass.simpleName, document.documents.toString())
+
+                val productList: ArrayList<Categories> = ArrayList()
+
+                for (i in document.documents) {
+                    val product = i.toObject(Categories::class.java)!!
+                    //product.categories_id = i.id
+
+                    productList.add(product)
+                }
+                when (activity) {
+                    is CategoryActivity -> activity.successCategoryListFromFirestore(productList)
+                    is DashboardActivity -> activity.successCategoryListFromFirestore(productList)
+
+                }
+            }
+            .addOnFailureListener { e ->
+                when (activity) {
+                    is CategoryActivity -> activity.hideProgressDialog()
+                }
+                Log.e("Error :: ", e.message.toString())
+            }
+    }
+
+    fun getDoctorList(activity: Activity, cat_id: Int) {
+        mFireStore.collection(Constants.DOCTOR)
+            .whereEqualTo(Constants.CATEGORIES_ID, cat_id)
+            .get()
+            .addOnSuccessListener { document ->
+                Log.i(activity.javaClass.simpleName, document.documents.toString())
+
+                val productList: ArrayList<Doctors> = ArrayList()
+
+                for (i in document.documents) {
+                    val product = i.toObject(Doctors::class.java)!!
+                    product.doctor_id = i.id
+                    productList.add(product)
+                }
+                when (activity) {
+                    is DoctorListActivity -> activity.successDoctorListFromFirestore(productList)
+                    is DashboardActivity -> activity.successDoctorListFromFirestore(productList)
+
+                }
+            }
+            .addOnFailureListener { e ->
+                when (activity) {
+                    is DoctorListActivity -> activity.hideProgressDialog()
+                    is DashboardActivity -> activity.hideProgressDialog()
+                }
+                Log.e("Error :: ", e.message.toString())
+            }
+    }
+
+    fun getAllDoctorList(activity: Activity, cat_id: Int) {
+        mFireStore.collection(Constants.DOCTOR)
+            .get()
+            .addOnSuccessListener { document ->
+                Log.i(activity.javaClass.simpleName, document.documents.toString())
+
+                val productList: ArrayList<Doctors> = ArrayList()
+
+                for (i in document.documents) {
+                    val product = i.toObject(Doctors::class.java)!!
+                    product.doctor_id = i.id
+                    productList.add(product)
+                }
+                when (activity) {
+                    is DoctorListActivity -> activity.successDoctorListFromFirestore(productList)
+                    is DashboardActivity -> activity.successDoctorListFromFirestore(productList)
+
+                }
+            }
+            .addOnFailureListener { e ->
+                when (activity) {
+                    is DoctorListActivity -> activity.hideProgressDialog()
+                    is DashboardActivity -> activity.hideProgressDialog()
+                }
+                Log.e("Error :: ", e.message.toString())
+            }
+    }
+
+
+    fun getTopDoctorList(activity: Activity, cat_id: Double) {
+        mFireStore.collection(Constants.DOCTOR)
+            .whereGreaterThan(Constants.RATING, cat_id)
+            .get()
+            .addOnSuccessListener { document ->
+                Log.i(activity.javaClass.simpleName, document.documents.toString())
+
+                val productList: ArrayList<Doctors> = ArrayList()
+
+                for (i in document.documents) {
+                    val product = i.toObject(Doctors::class.java)!!
+                    product.doctor_id = i.id
+                    productList.add(product)
+                }
+                when (activity) {
+                    is DoctorListActivity -> activity.successDoctorListFromFirestore(productList)
+                    is DashboardActivity -> activity.successDoctorListFromFirestore(productList)
+
+                }
+            }
+            .addOnFailureListener { e ->
+                when (activity) {
+                    is DoctorListActivity -> activity.hideProgressDialog()
+                    is DashboardActivity -> activity.hideProgressDialog()
+                }
+                Log.e("Error :: ", e.message.toString())
+            }
+    }
+
+    fun geDoctorDetails(activity: DocotrDetailActivity, doctorID: String) {
+        activity.showProgressDialog("")
+        mFireStore.collection(Constants.DOCTOR)
+            .document(doctorID)
+            .get()
+            .addOnSuccessListener { document ->
+                Log.i(activity.javaClass.simpleName, document.toString())
+
+                val product = document.toObject(Doctors::class.java)
+
+                if (product != null) {
+                    activity.doctorDetailsSuccess(product)
+                }
+            }
+            .addOnFailureListener { e ->
+                activity.hideProgressDialog()
+                Log.e(activity.javaClass.simpleName, e.message.toString())
+            }
+    }
+
+    fun addBooking(activity: DocotrDetailActivity, user: Booking) {
+        mFireStore.collection(BOOKING)
+            .document(user.booking_id)
+            .set(user, SetOptions.merge())
+            .addOnSuccessListener {
+                activity.userBookingSuccess()
+            }.addOnFailureListener { exception ->
+                activity.hideProgressDialog()
+                Log.e(activity.javaClass.simpleName, exception.message.toString())
+            }
+
+    }
+
+    fun getMyBookingList(activity: MyBookingActivity, uer_id: String) {
+        activity.showProgressDialog("")
+        mFireStore.collection(Constants.BOOKING)
+            .whereEqualTo(Constants.USER_ID, uer_id)
+            .get()
+            .addOnSuccessListener { document ->
+                Log.i(activity.javaClass.simpleName, document.documents.toString())
+
+                val productList: ArrayList<Booking> = ArrayList()
+
+                for (i in document.documents) {
+                    val product = i.toObject(Booking::class.java)!!
+                    product.booking_id = i.id
+                    productList.add(product)
+                }
+                activity.successBookingListFromFirestore(productList)
+            }
+            .addOnFailureListener { e ->
+                activity.hideProgressDialog()
+                Log.e("Error :: ", e.message.toString())
+            }
+    }
+}
--- app/src/main/java/com/bookmydoc/view/DashboardActivity.kt
+++ app/src/main/java/com/bookmydoc/view/DashboardActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bookmydoc.R
 import com.bookmydoc.adapter.CategoryAdapter
 import com.bookmydoc.adapter.DoctorAdapter
@@ -12,12 +14,52 @@ import com.bookmydoc.base.BaseActivity
 import com.bookmydoc.databinding.ActivityCategoryBinding
 import com.bookmydoc.databinding.ActivityDashboardBinding
 import com.bookmydoc.databinding.ActivityDocotrDetailBinding
+import com.bookmydoc.firestore.FireStoreClass
+import com.bookmydoc.interfaces.ListSelector
+import com.bookmydoc.model.Categories
+import com.bookmydoc.model.Doctors
+import com.bookmydoc.model.User
+import com.bumptech.glide.Glide
 import java.util.ArrayList
 
 class DashboardActivity : BaseActivity(), View.OnClickListener {
     private var doctorAdapyter: DoctorAdapter? = null
-    private var doctorArrayList = ArrayList<String>()
+    private var doctorArrayList = ArrayList<Doctors>()
     private lateinit var binding: ActivityDashboardBinding
+
+    private var categoryAdapter: CategoryAdapter? = null
+    private lateinit var categoryArrayList: ArrayList<Categories>
+
+    var name = arrayOf(
+        "Dr. Bill Dorfman",
+        "Dr. Don Solooki",
+        "Dr. Jay R. Lieberman",
+        "Dr. Mark Spoonamore"
+    )
+    var categories = arrayOf(
+        "Aesthetic Dentistry",
+        "DDS, PhD, Dentist",
+        "Orthopedic surgeon",
+        "Orthopedic surgeon"
+    )
+    var categories_id = arrayOf(4, 4, 6, 6)
+    var patient = arrayOf("18K", "2k", "5K", "7K")
+    var experience = arrayOf("10", "5", "7", "8")
+    var rating = arrayOf(4.5, 4.5, 4.0, 4.9)
+    var mobile_number = arrayOf("+13102775678", "2136243333", "+18008722273", "+18008722273")
+    var latitude = arrayOf(
+        "40.730817928533725",
+        "34.050146118647326",
+        "34.06288944377939",
+        "34.06284562799231"
+    )
+    var longitude = arrayOf(
+        "-74.06492554226466",
+        "-118.25371249231205",
+        "-118.20267052573662",
+        "-118.20282822659448"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_dashboard)
@@ -28,17 +70,59 @@ class DashboardActivity : BaseActivity(), View.OnClickListener {
         binding.cardBrain.setOnClickListener(this)
         binding.cardDental.setOnClickListener(this)
         binding.cardTopDoctor.setOnClickListener(this)
+        binding.imgProfile.setOnClickListener(this)
 
 
+        for (item in name.indices) {
 
+            val doctors = Doctors(
+                System.currentTimeMillis().toString(),
+                name[item],
+                categories[item],
+                categories_id[item],
+                patient[item],
+                experience[item],
+                rating[item],
+                mobile_number[item],
+                latitude[item],
+                longitude[item]
+            )
+            //FireStoreClass().addDoctor(this, doctors)
+        }
+        getUserDetails()
+        FireStoreClass().getTopDoctorList(this, 4.0)
     }
 
+    fun successDoctorListFromFirestore(doctorArrayList: ArrayList<Doctors>) {
+        this.doctorArrayList = doctorArrayList
+        doctorAdapyter = DoctorAdapter(object : ListSelector {
+            override fun selectedList(position: Int) {
+
+                val intent = Intent(this@DashboardActivity, DocotrDetailActivity::class.java)
+                intent.putExtra("drId", doctorArrayList.get(position).doctor_id)
+                startActivity(intent)
+            }
+
+        }, 3)
+        binding.rvDoctors.layoutManager = LinearLayoutManager(this)
+        binding.rvDoctors.adapter = doctorAdapyter
+        doctorAdapyter!!.setUpcomingList(this, doctorArrayList)
+
+        if(doctorArrayList.size>0){
+            binding.txtDrName.text = doctorArrayList.get(0).name
+            binding.txtCategory.text = doctorArrayList.get(0).categories
+        }
+    }
+    fun userRegistrationSuccess() {}
     override fun onClick(view: View?) {
         when (view) {
             binding.linPopular -> {
-                val intent = Intent(this, DocotrDetailActivity::class.java)
-                intent.putExtra("drName", "Dr. Patrick Cruz")
-                startActivity(intent)
+                if(doctorArrayList.size>0){
+                    val intent = Intent(this@DashboardActivity, DocotrDetailActivity::class.java)
+                    intent.putExtra("drId", doctorArrayList.get(0).doctor_id)
+                    startActivity(intent)
+                }
+
             }
             binding.cardTopDoctor -> {
                 val intent = Intent(this, DocotrDetailActivity::class.java)
@@ -52,23 +136,55 @@ class DashboardActivity : BaseActivity(), View.OnClickListener {
             binding.txtDrViewMore -> {
                 val intent = Intent(this, DoctorListActivity::class.java)
                 intent.putExtra("category", "Top Doctors")
+                intent.putExtra("categoryId", 0)
                 startActivity(intent)
             }
-            binding.cardBrain -> {
-                val intent = Intent(this, DoctorListActivity::class.java)
-                intent.putExtra("category", "Brain")
-                startActivity(intent)
-            }
-            binding.cardDental -> {
-                val intent = Intent(this, DoctorListActivity::class.java)
-                intent.putExtra("category", "Dental")
-                startActivity(intent)
-            }
-            binding.cardEye -> {
-                val intent = Intent(this, DoctorListActivity::class.java)
-                intent.putExtra("category", "Eye")
+
+            binding.imgProfile -> {
+                val intent = Intent(this, SettingsActivity::class.java)
                 startActivity(intent)
             }
         }
     }
+
+    private fun getUserDetails() {
+        showProgressDialog(getString(R.string.please_wait))
+        FireStoreClass().getUserDetails(this)
+    }
+
+    fun userDetailSuccess(user: User) {
+
+        getCategorisList()
+        hideProgressDialog()
+        binding.txtUserName.text = "${user.fullName}"
+        Glide.with(this)
+            .load(user.image)
+            .into(binding.imgProfile)
+    }
+
+    private fun getCategorisList() {
+        FireStoreClass().getCategoriesList(this@DashboardActivity)
+    }
+
+    fun successCategoryListFromFirestore(productList: ArrayList<Categories>) {
+        hideProgressDialog()
+        categoryArrayList = productList
+        categoryAdapter = CategoryAdapter(object : ListSelector {
+            override fun selectedList(position: Int) {
+
+                val intent = Intent(this@DashboardActivity, DoctorListActivity::class.java)
+                intent.putExtra("category", categoryArrayList.get(position).name)
+                intent.putExtra("categoryId", categoryArrayList.get(position).id)
+                startActivity(intent)
+            }
+
+        }, 3)
+        val layoutManager3 = GridLayoutManager(this, 3)
+        binding.rvMorningSlot.layoutManager = layoutManager3
+        binding.rvMorningSlot.adapter = categoryAdapter
+        categoryAdapter!!.setUpcomingList(this, categoryArrayList)
+
+
+    }
+
 }
